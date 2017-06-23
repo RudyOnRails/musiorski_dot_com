5201ebd618b401d4ace09d61501d3ef21077ad6d
Kevin Musiorski <rudyonrails@gmail.com>
2017-06-23 06:22:52 +0000

    redirecting to google doc resume for now to test out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,8 @@ class PagesController < ApplicationController
   end
 
   def resume
-    redirect_to "/musiorski_resume.pdf"
+    # redirect_to "/musiorski_resume.pdf"
+    redirect_to "https://docs.google.com/document/d/1mjxUEJtAbt_pptqwq8_M4ZBg3zJEZq_QgXPX9UBt0L0/edit?usp=sharing"
   end
 
   def chitech_digital_portfolio
